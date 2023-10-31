8b31918258c700702667c77a04d59c88363b9397
John-Davidson-8 <jodo828@gmail.com>
2023-10-31 09:37:54 +0000

    Add task 2 and 3

--- test.js
+++ test.js
@@ -1,6 +1,24 @@
-function add7(number) {
-  return number + 7;
-}
+// Task 1
+// function add7(number) {
+//   return number + 7;
+// }
 
-let result = add7(3);
-console.log(result);
+// let result = add7(3);
+// console.log(result);
+
+// Task 2
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+
+// let result = multiply(5, 10);
+// console.log(result); // Output: 50
+
+// Task 3
+// function capitalize(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+// }
+
+// console.log(capitalize("hello")); // Output: "Hello"
+// console.log(capitalize("WORLD")); // Output: "World"
+// console.log(capitalize("MIXeD")); // Output: "Mixed"
